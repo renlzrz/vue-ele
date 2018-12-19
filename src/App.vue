35bfa38e2aa64c48953db83ee674abb27ddebba5
千残月 <438955580@qq.com>
2018-12-19 15:33:49 +0800

    2018-12-19 16:10

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <router-view/>
+    <router-view name="app"></router-view>
   </div>
 </template>
 
@@ -10,7 +10,7 @@ export default {
   name: 'App'
 }
 </script>
-   
+
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
