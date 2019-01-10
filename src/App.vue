9bc8007bd663e46c5d189ebb37021e3443a6ff95
renlzrz <438955580@qq.com>
2019-01-10 15:26:14 +0800

    2019-1-10 12:10

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
 </template>
 
 <script>
-export default {  
+export default {
   name: 'App'
 }
 </script>
@@ -14,7 +14,7 @@ export default {
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
+  -webkit-font-smoothing:   antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
